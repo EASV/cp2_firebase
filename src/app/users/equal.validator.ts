f27b10279e59758d4c7d266567d49425e64357db
lbilde <ljb@easv.dk>
2017-03-09 00:19:10 +0100

    added validation directive

--- src/app/users/equal.validator.ts
+++ src/app/users/equal.validator.ts
@@ -1,7 +1,6 @@
-import {Directive, forwardRef, OnDestroy} from '@angular/core';
+import {Directive, forwardRef} from '@angular/core';
 import { NG_VALIDATORS, Validator, AbstractControl } from '@angular/forms';
 import { Input } from "@angular/core";
-import {Subscription} from "rxjs";
 
 @Directive({
   selector: '[validateEqualTo][ngModel],[validateEqualTo][formControlName]',
@@ -9,44 +8,17 @@ import {Subscription} from "rxjs";
     {provide: NG_VALIDATORS, useExisting: forwardRef(() => EqualTextValidator), multi: true}
   ]
 })
-export default class EqualTextValidator implements Validator, OnDestroy {
+export default class EqualTextValidator implements Validator {
   @Input() validateEqualTo : string;
-  private _onChange : () => void;
-  oldVal : string;
-  sub: Subscription;
 
-  registerOnValidatorChange( fn : () => void ) {
-    this._onChange = fn;
-  }
-
-  /**
-   *
-   * @param c
-   * @returns {{validateEqual: boolean}|null}
-   */
   validate( c : AbstractControl ) {
-    let passwordVal = c.value;
-    let repeatEle = c.root.get(this.validateEqualTo);
-    if( repeatEle ) {
-      this.oldVal = repeatEle.value;
-    }
-    return this.checkEquality(passwordVal, repeatEle);
+    let inputValue = c.value;
+    let otherValue = c.root.get(this.validateEqualTo);
+    return this.checkEquality(inputValue, otherValue);
   }
 
-  ngOnDestroy(){
-    if(this.sub){
-      this.sub.unsubscribe();
-    }
-  }
-
-  /**
-   *
-   * @param passwordVal
-   * @param repeatEle
-   * @returns {any}
-   */
-  checkEquality( passwordVal: string, repeatEle: any ) {
-    if( repeatEle && passwordVal !== repeatEle.value ) return {
+  checkEquality( inputValue: string, otherValue: any ) {
+    if( otherValue && inputValue !== otherValue.value ) return {
       validateEqual: true
     }
     return null;
