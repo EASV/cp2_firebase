35087b556f2120add5a1a45b790320cd2b4f47fd
lbilde <ljb@easv.dk>
2017-03-26 22:42:38 +0200

    added animation to user list

--- src/app/users/user-list/user-list.component.ts
+++ src/app/users/user-list/user-list.component.ts
@@ -1,4 +1,11 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {
+  Component, OnInit, Input,
+  trigger,
+  state,
+  style,
+  transition,
+  animate, keyframes
+} from '@angular/core';
 import {Observable} from "rxjs";
 import {User} from "../user";
 import {UserService} from "../user.service";
@@ -6,7 +13,38 @@ import {UserService} from "../user.service";
 @Component({
   selector: 'cp-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.css'],
+  animations: [
+    trigger('flyInOut', [
+      state('adm', style({transform: 'translateY(0)'})),
+      state('edu', style({transform: 'translateY(0)'})),
+      state('stud', style({transform: 'translateX(0)'})),
+      transition('void => adm', [
+        style({transform: 'translateY(100%)'}),
+        animate(100)
+      ]),
+      transition('adm => void', [
+        animate(100, style({transform: 'translateX(100%)'}))
+      ]),
+      transition('void => edu', [
+        animate(800, keyframes([
+          style({opacity: 1, transform: 'translateY(-100%)'}),
+          style({opacity: 1, transform: 'translateY(-100px)'}),
+          style({opacity: 0, transform: 'translateY(10px)'})
+        ]))
+      ]),
+      transition('edu => void', [
+        animate(800, style({transform: 'translateY(100%)'}))
+      ]),
+      transition('void => stud', [
+        style({transform: 'translateX(-100%)'}),
+        animate(1100)
+      ]),
+      transition('stud => void', [
+        animate(1100, style({transform: 'translateY(-100%)'}))
+      ])
+    ])
+  ]
 })
 export class UserListComponent implements OnInit {
 
